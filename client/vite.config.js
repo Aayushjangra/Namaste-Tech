700f51969b8f43efe729c912b7dd5c3bb5291df6
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2024-11-18 20:41:28 +0530

    login and signup page

---
+++ client/vite.config.js
@@ -0,0 +1,31 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import path from "path";
+import { fileURLToPath } from "url"; // Import this to work with `import.meta.url`
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// Fix for `__dirname` in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
---
+++ server/utils/generateToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (res, user, message) => {
+    //jwt ka sign method hota hai token generate sign karne ke leye
+  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return res
+    .status(200)
+    .cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    }).json({
+        success:true,
+        message,
+        user
+    });
+};
