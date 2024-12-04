9d5901206148a9d736d274e367c09f9f8ce5f594
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2024-12-04 14:18:00 +0530

    course progress

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,13 +3,14 @@ import rootRedcuer from "./rootReducer";
 import { authApi } from "@/features/api/authApi";
 import { courseApi } from "@/features/api/courseApi";
 import { purchaseApi } from "@/features/api/purchaseApi";
+import { courseProgressApi } from "@/features/api/courseProgressApi";
 
 
 
 
 export const appStore = configureStore({
   reducer: rootRedcuer,
-  middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware, courseApi.middleware,purchaseApi.middleware)
+  middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware, courseApi.middleware,purchaseApi.middleware, courseProgressApi.middleware)
 });
 
 
